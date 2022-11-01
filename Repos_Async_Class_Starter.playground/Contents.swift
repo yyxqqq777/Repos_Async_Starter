322ab9d68c0d8ed689e6cbd63196b5300bbde95b
yyxqqq777 <yunxuan517@gmail.com>
2022-11-01 11:54:40 -0400

    feat(repos): loop over repos array to print name and url

--- Repos_Async_Class_Starter.playground/Contents.swift
+++ Repos_Async_Class_Starter.playground/Contents.swift
@@ -21,9 +21,9 @@ struct Repository: Decodable {
 }
 
 // Our initial function (needs fixing...)
-func fetchRepositories() throws -> [Repository] {
+func fetchRepositories() async throws -> [Repository] {
   let url = URL(string: "https://api.github.com/search/repositories?q=language:swift&sort=stars&order=desc")!
-  let (data, _) = try URLSession.shared.data(from: url)
+  let (data, _) = try await URLSession.shared.data(from: url)
   return try JSONDecoder().decode(Repositories.self, from: data).repos
 }
 
@@ -32,7 +32,19 @@ func fetchRepositories() throws -> [Repository] {
 // - call function then loop over repos array to print out the name & url for each
 // - interject lots of print statements with 'Step X' to see how things are progressing
 Task {
-  print("Step 1")
+  print("Step 1: task starts")
+  do {
+    print("Step 2: fetch the repo")
+    let repoArray:[Repository] = try await fetchRepositories()
+    print("Step 3: loop over repos")
+    for repo in repoArray {
+      print("- \(repo.name): \(repo.htmlURL)")
+    }
+    print("step4: task ends")
+  } catch {
+    print(error)
+  }
+  
 
   // ...
 
